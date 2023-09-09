56f1e66b021973bdda5df07c0dbceadbff841204
Carol Pereira <130416799+carol9074@users.noreply.github.com>
2023-09-08 21:29:38 -0300

    Add files via upload

--- aulas_array/v05.php
+++ aulas_array/v05.php
@@ -1,17 +1,29 @@
 <?php
 
+$num;
 $mes = [
-    "janeiro",
-    "fevereiro", "março", "abril", "maio", "junho",
-    "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+   1 => "janeiro",
+   2 => "fevereiro", 
+   3 => "março",
+   4 => "abril", 
+   5 => "maio", 
+   6 => "junho",
+   7 => "julho",
+   8 => "agosto", 
+   9 => "setembro",
+   10 => "outubro",
+   11 => "novembro",
+   12 => "dezembro"];
 
-for ($i = 0; $i < 12; $i++) {
-    echo "Informe um número";
+
+    echo "Informe um número:";
     $num = readline();
+
+if(isset($mes [$num] )){
+$res = $mes[$num];
+echo "O mes corresponde ao número sitado é $res";
+}else{
+    echo "Número inválido";
 }
 
-    for($i = 0; $i < 10; $i++){
-
-      
-    }
-
+?>
