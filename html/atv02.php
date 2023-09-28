5aa915eba196273a6bf34a7802e457b677f13f1d
Carol Pereira <130416799+carol9074@users.noreply.github.com>
2023-09-28 16:11:58 -0300

    Add files via upload

---
+++ html/atv02.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de tarefas</title>
+</head>
+<body>
+
+<h1>Minha lista do dia</h1>
+<ul>
+
+<li>Acordar cedo</li>
+<li>Tomar café</li>
+<li>Fazer as compras no mercado</li>
+<li>Arrumar a casa</li>
+<li>Fazer o almoço </li>
+<li>Tomar banho</li>
+<li>Ir trabalhar</li>
+
+</ul>
+    
+</body>
+</html>
