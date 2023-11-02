7e2fe931fd18ba2df6acd674a1c7b11e616f87c6
Carol Pereira <130416799+carol9074@users.noreply.github.com>
2023-11-01 22:01:35 -0300

    Add files via upload

---
+++ PHPDB/novo-user.php
@@ -0,0 +1,28 @@
+<h1>Cadastro de usuario</h1>
+
+<form>
+
+<div class="mb-3">
+<label>Nome</label>
+<input type="text" name="nome" class="form-control">
+</div>
+
+<div class="mb-3">
+<label>Email</label>
+<input type="text" name="email" class="form-control">
+</div>
+
+<div class="mb-3">
+<label>Endereço</label>
+<input type="text" name="ende" class="form-control">
+</div>
+
+<div class="mb-3">
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</div>
+
+
+
+
+
+</form>
